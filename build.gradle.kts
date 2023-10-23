f36a49851a6581f5181a5ae32ada12fd58a46298
Towich <towichf@gmail.com>
2023-10-23 22:57:56 +0300

    feat: add json Serialization ~ GET(127.0.0.1:8080/getOwner) -> returns owner&text

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     application
     kotlin("jvm") version "1.9.10"
     id("io.ktor.plugin") version "2.3.5"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10"
 }
 
 group = "com.towich"
@@ -27,6 +28,7 @@ dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("io.ktor:ktor-serialization-kotlinx-json")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
--- src/main/kotlin/com/towich/plugins/Routing.kt
+++ src/main/kotlin/com/towich/plugins/Routing.kt
@@ -3,11 +3,19 @@ package com.towich.plugins
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
+import java.util.UUID
+
+@Serializable
+data class Test(
+    val owner: String,
+    val text: String
+)
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        get("/getOwner") {
+            call.respond(Test(owner = "Towich", text = UUID.randomUUID().toString()))
         }
     }
 }
